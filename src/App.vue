2110042aa5d84401261135734a2229240fb5ed11
tianhe <9932705+zhuyiwenyuntianhe@user.noreply.gitee.com>
2022-03-06 16:16:37 +0800

    '第二次提交-vant组件库方面'

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <!-- 路由的出口 -->
-    <router-view/>
-    <h1>黑马头条</h1> 
+    <router-view />
+    <h1>黑马头条</h1>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 使用的是方式三: 导入所有的组件,但会增加代码的体积,不太推荐这样的做法
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+
+// 加载全局样式
 import './styles/index.less'
+
+// 注册使用 Vant 组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
